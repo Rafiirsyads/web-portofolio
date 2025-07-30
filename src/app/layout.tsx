6c4eafb10974b59b6724288b43374dc747d4913e
Rafiirsyads <rafiirsyad2004@gmail.com>
2025-07-30 09:41:37 +0700

    feat: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={`bg-background text-white ${inter.className}`}>{children}</body>
+      <body className={`bg-background text-white ${inter.className}`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,21 @@ import Contact from "@/components/Contact";
 export default function Home() {
   return (
     <main className="relative overflow-x-hidden">
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+      <div id="home">
+        <Hero />
+      </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
     </main>
   );
 }
